2288da77ae66bdf220b8a49dfb433959eeee66c3
Akindu Kodithuwakku <akindukodithuwakku@gmail.com>
2025-05-13 23:05:56 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Travel Planner</h1>
+      <Button>Click me</Button>
+     
+    </>
+  )
+}
+
+export default App
